7875ee4562666a0ec1d5d304342371424c4c8fad
Tomi <developer@livestudio.eu>
2020-12-12 17:39:46 +0100

    header and item respo fix

--- src/components/shop/Item.js
+++ src/components/shop/Item.js
@@ -9,9 +9,9 @@ const Item = (props) => {
 		<div className="item mb-3" onClick={() => props.toggle(props.data)}>
 			<div className="d-flex flex-column flex-md-row align-items-stretch justify-content-between">
 				<div className="text-content flex-grow-1 p-3 order-2 order-md-1">
-					<div className="d-flex justify-content-between">
-						<p className="font-weight-bold">{props.data.name}</p>
-						<div className="raiting text-right">
+					<div className="d-flex flex-column flex-md-row justify-content-md-between">
+						<p className="font-weight-bold mb-1 mb-md-0">{props.data.name}</p>
+						<div className="raiting text-md-right mb-2 mb-md-0">
 							<FontAwesomeIcon icon={faStar} />
 							<FontAwesomeIcon icon={faStar} />
 							<FontAwesomeIcon icon={faStar} />
@@ -23,11 +23,11 @@ const Item = (props) => {
 						</div>
 					</div>
 					<p className="mb-2 text-black-50">{props.data.cal} Cal.</p>
-					<div className="d-flex justify-content-between align-items-center">
+					<div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
 						<p className="text-black-50 text-sm mb-0">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...
 						</p>
-						<h5 className="font-weight-bold text-success mb-0">
+						<h5 className="font-weight-bold text-success mb-0 mt-3 mt-md-0">
 							${parseFloat(props.data.price).toFixed(2)}
 						</h5>
 					</div>
