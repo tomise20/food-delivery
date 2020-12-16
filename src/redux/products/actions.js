b73cd527ba2ce9debda576778bb1cb5bba9a350a
Tomi <developer@livestudio.eu>
2020-12-16 08:01:06 +0100

    update product actions

--- src/redux/products/actions.js
+++ src/redux/products/actions.js
@@ -1,8 +1,8 @@
 import { MODIFY_ITEM } from "./actionTypes";
 
-export const modifyItem = (itemId, products) => {
+export const modifyItem = (itemId, products, action = true) => {
 	let index = products.findIndex((data) => itemId === data.id);
-	products[index].isCart = true;
+	products[index].isCart = action;
 	return {
 		type: MODIFY_ITEM,
 		payload: products,
