fc472330060cc744a07b20360a2b45f99b03eada
Tomi <developer@livestudio.eu>
2020-12-19 14:51:58 +0100

    fix route problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./scense/home/Home";
 import "./App.scss";
@@ -7,7 +7,6 @@ import Shop from "./scense/Shop/Shop";
 import CartPage from "./scense/cart/CartPage";
 import Login from "./scense/auth/Login";
 import AuthLayout from "./layouts/AuthLayout";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -17,26 +16,24 @@ import PageNotFound from "./scense/404/PageNotFound";
 export default function App() {
 	return (
 		<Provider store={store}>
-			<BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
-				<Fragment>
-					<Switch>
-						<Route exact path={["/", "/shop", "/cart"]}>
-							<MainLayout>
-								<Route path="/shop" component={Shop} />
-								<Route path="/cart" component={CartPage} />
-								<Route exact path="/" component={Home} />
-							</MainLayout>
-						</Route>
+			<Router basename={process.env.REACT_APP_PUBLIC_URL}>
+				<Switch>
+					<Route exact path={["/", "/shop", "/cart"]}>
+						<MainLayout>
+							<Route path="/shop" component={Shop} />
+							<Route path="/cart" component={CartPage} />
+							<Route exact path="/" component={Home} />
+						</MainLayout>
+					</Route>
 
-						<Route exact path={["/signin"]}>
-							<AuthLayout>
-								<Route path="/signin" component={Login} />
-							</AuthLayout>
-						</Route>
-						<Route path="*" component={PageNotFound} />
-					</Switch>
-				</Fragment>
-			</BrowserRouter>
+					<Route exact path={["/signin"]}>
+						<AuthLayout>
+							<Route path="/signin" component={Login} />
+						</AuthLayout>
+					</Route>
+					<Route path="*" component={PageNotFound} />
+				</Switch>
+			</Router>
 		</Provider>
 	);
 }
