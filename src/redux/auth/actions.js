22f2de189282b917460e42d594710b5ccd286e46
Tomi <developer@livestudio.eu>
2021-01-21 08:39:07 +0100

    add getUser and address functions

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,5 +1,16 @@
 import axios from "axios";
-import { REQUEST_AUTH_LOGIN, SUCCESS_AUTH_LOGIN, FAILED_AUTH_LOGIN, AUTH_SIGN_OUT } from "./actionTypes";
+import {
+	REQUEST_AUTH_LOGIN,
+	SUCCESS_AUTH_LOGIN,
+	FAILED_AUTH_LOGIN,
+	AUTH_SIGN_OUT,
+	ADD_ADDRESS,
+	SET_ACTIVE_ADDRESS,
+	REQUEST_GET_USER,
+	SUCCESSFULLY_GET_USER,
+	FAILED_GET_USER,
+	DELETE_ADDRESS,
+} from "./actionTypes";
 
 export const requestAuthLogin = () => {
 	return {
@@ -28,11 +39,12 @@ export const authLogin = (username, password) => {
 		axios
 			.post(`${process.env.REACT_APP_SERVER_URL}/login`, credentials)
 			.then((res) => {
-				console.log(res.data);
 				let user = res.data.user;
 				user.accessToken = res.data.access_token.token;
 				delete user.password;
-
+				let expire = new Date();
+				expire.setTime(expire.getTime() * 3600 * 1000 * 24 * 365 * 10);
+				document.cookie = `token=${user.accessToken}; expires=${expire.toUTCString()}; path="/";`;
 				dispatch(successAuthLogin(user));
 			})
 			.catch((error) => {
@@ -41,6 +53,111 @@ export const authLogin = (username, password) => {
 					console.log("lefut");
 					dispatch(failedAuthLogin(errorMsg));
 				}
+			});
+	};
+};
+
+export const requestGetUser = () => {
+	return {
+		type: REQUEST_GET_USER,
+	};
+};
+
+export const successfullyGetUser = (user) => {
+	return {
+		type: SUCCESSFULLY_GET_USER,
+		payload: user,
+	};
+};
+
+export const failedGetUser = (error) => {
+	return {
+		type: FAILED_GET_USER,
+		payload: error,
+	};
+};
+
+export const getUser = (token) => {
+	return (dispatch) => {
+		dispatch(requestGetUser());
+
+		axios
+			.get(`${process.env.REACT_APP_SERVER_URL}/user/get-user`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				dispatch(successAuthLogin(res.data));
+			})
+			.catch((error) => {
+				const errorMsg = error.response.data.message;
+				dispatch(failedGetUser(error.message));
+			});
+	};
+};
+
+export const addAddress = (address) => {
+	return {
+		type: ADD_ADDRESS,
+		payload: address,
+	};
+};
+
+export const successfullySetActiveAddress = (addresses) => {
+	return {
+		type: SET_ACTIVE_ADDRESS,
+		payload: addresses,
+	};
+};
+
+export const setActiveAddress = (userId, addressId, token) => {
+	return (dispatch) => {
+		axios
+			.post(
+				`${process.env.REACT_APP_SERVER_URL}/user/set-active-address`,
+				{
+					user_id: userId,
+					address_id: addressId,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			)
+			.then((res) => {
+				const addresses = res.data.addresses;
+				dispatch(successfullySetActiveAddress(addresses));
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	};
+};
+
+export const successfullyDeleteAddress = (addresses) => {
+	return {
+		type: DELETE_ADDRESS,
+		payload: addresses,
+	};
+};
+
+export const deleteAddress = (addresses, addressId, token) => {
+	return (dispatch) => {
+		axios
+			.delete(`${process.env.REACT_APP_SERVER_URL}/user/${addressId}/delete-address`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => {
+				const newAddresses = addresses.filter((address) => address.id !== addressId);
+				dispatch(successfullyDeleteAddress(newAddresses));
+			})
+			.catch((err) => {
+				console.log(err.message);
 			});
 	};
 };
--- src/redux/auth/authReducer.js
+++ src/redux/auth/authReducer.js
@@ -1,4 +1,15 @@
-import { REQUEST_AUTH_LOGIN, SUCCESS_AUTH_LOGIN, FAILED_AUTH_LOGIN, AUTH_SIGN_OUT } from "./actionTypes";
+import {
+	REQUEST_AUTH_LOGIN,
+	SUCCESS_AUTH_LOGIN,
+	FAILED_AUTH_LOGIN,
+	AUTH_SIGN_OUT,
+	ADD_ADDRESS,
+	SET_ACTIVE_ADDRESS,
+	REQUEST_GET_USER,
+	SUCCESSFULLY_GET_USER,
+	FAILED_GET_USER,
+	DELETE_ADDRESS,
+} from "./actionTypes";
 
 const initialState = {
 	user: {
@@ -39,12 +50,58 @@ const AuthReducer = (state = initialState, action) => {
 				error: action.payload,
 			};
 		}
+		case REQUEST_GET_USER: {
+			return {
+				...state,
+				loading: true,
+			};
+		}
+		case SUCCESSFULLY_GET_USER: {
+			return {
+				...state,
+				user: action.payload,
+				isLoggedIn: true,
+			};
+		}
+		case FAILED_GET_USER: {
+			return {
+				...state,
+				error: action.error,
+			};
+		}
+		case DELETE_ADDRESS: {
+			return {
+				...state,
+				user: {
+					...state.user,
+					addresses: action.payload,
+				},
+			};
+		}
 		case AUTH_SIGN_OUT: {
 			return {
 				...state,
 				loading: false,
 				user: {},
 				isLoggedIn: false,
+			};
+		}
+		case ADD_ADDRESS: {
+			return {
+				...state,
+				user: {
+					...state.user,
+					addresses: [...state.user.addresses, action.payload.address],
+				},
+			};
+		}
+		case SET_ACTIVE_ADDRESS: {
+			return {
+				...state,
+				user: {
+					...state.user,
+					addresses: action.payload,
+				},
 			};
 		}
 		default:
