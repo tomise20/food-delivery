05ae2e6e303e8570eaf057ccf42c28053f18a40c
Tomi <developer@livestudio.eu>
2021-01-21 08:38:07 +0100

    add getUser function

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,17 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import Chat from "../components/Chat/Chat";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import Snackbar from "../components/snackbar/Snackbar";
+import { connect } from "react-redux";
+import { getUser } from "../redux/auth/actions";
+import { useCookies } from "react-cookie";
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ getUser, auth, children }) => {
+	const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+	const token = cookies.token;
+
+	useEffect(() => {
+		async function checkUser() {
+			if (token !== undefined && auth.isLoggedIn === false) {
+				await getUser(token);
+			}
+		}
+		checkUser();
+	}, []);
+
 	return (
 		<Fragment>
 			<Header />
 			<Snackbar />
+			<Chat />
 			{children}
 			<Footer />
 		</Fragment>
 	);
 };
 
-export default MainLayout;
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getUser })(MainLayout);
